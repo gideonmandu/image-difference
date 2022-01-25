b650d06c7b5c15d2d055aabaa538a3ec05bce84a
gideonmandu <gideonmandu@gmail.com>
2022-01-25 20:08:35 +0300

    setup api routes

--- backend/src/api/routes.py
+++ backend/src/api/routes.py
@@ -1,21 +1,24 @@
+import uuid
 from typing import Optional
-from fastapi import UploadFile, File, APIRouter
+from fastapi import UploadFile, APIRouter
 from src.services.image_processing import ImageProcessor
 
-router = APIRouter(prefix='/file',tags=['passport & ID upload'])
+router = APIRouter(prefix="/file", tags=["passport & ID upload"])
 
 
-@router.get('')
+@router.get("")
 async def test():
-    return {"test":"nada"}
+    return {"test": "nada"}
 
 
 @router.post("/upload/")
-async def create_upload_file(file: Optional[UploadFile] = File(None)):
+async def create_upload_file(file: Optional[UploadFile] = None):
     if not file:
         return {"message": "No upload file sent"}
-    print(file)
-    image = file.file.read()
-    # TODO fix image being read
-    image_processor = ImageProcessor(image=image)
-    return {"filename": file.filename, "Imagetype":image_processor.image_type()}
+    file.filename = f"{uuid.uuid4()}.{file.filename.split('.')[1]}"
+    image_bytes = await file.read()
+
+    with open(f"{file.filename}", "wb") as f:
+        f.write(image_bytes)
+    image_processor = ImageProcessor(image=f"{file.filename}")
+    return {"filename": file.filename, "Imagetype": image_processor.image_type()}
